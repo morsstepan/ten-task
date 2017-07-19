90c61dfd4679d72b483c52d6d0741d5d8bd04fe1
stepan.morozov <stepan.mors@gmail.com>
2017-07-19 23:47:44 +0300

    third commit

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 import helpers.helper as helper
-import modules.reading_module as read
+# import modules.reading_module as read
 
 
 def main():
-    def printGreeting():
-        print('This program was created for scraping web-sites.')
-    printGreeting()
-    try:
-        article = read.readData()
-        article.download()
-        # print(a)
-        article.parse()
-        article.saveArticle()
-    except helper.ArticleException:
-        print('You need to input a correct website. \nCheck your input data.')
-        exit()
+	def printGreeting():
+		print('This program was created for scraping web-sites.')
+	printGreeting()
+
+	def readData():
+		if len(helper.sys.argv) == 1:
+			print('You need to specify the web-site')
+			exit()
+		elif len(helper.sys.argv) >= 1:
+			processUrl()
+
+	def processUrl():
+		for arg in helper.sys.argv[1:]:
+			print('Scraping from ' + str(arg) + '...')
+			article = helper.MyArticle(arg, language='ru')
+			article.download()
+			# print(article)
+			article.parse()
+			article.saveArticle()
+
+	try:
+		readData()
+	except helper.ArticleException:
+		print('You need to input a correct website. \nCheck your input data.')
+		exit()
 
 
 main()
